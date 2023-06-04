2305170ef1d597f74a9aae0f5a8fbed523df7ac8
CreatineEnjoyer <piotrdrobina@gmail.com>
2023-06-04 21:23:06 +0200

    Fixing_chest_items_04_06_2023_21_23

--- Assets/Scripts/Object/OpenedChest.cs
+++ Assets/Scripts/Object/OpenedChest.cs
@@ -17,6 +17,7 @@ public class OpenedChest : MonoBehaviour
     {
         gameObject.GetComponent<SpriteRenderer>().sprite = openedChest;
         Instantiate(chestItem, new Vector3(transform.position.x, (transform.position.y + 1), transform.position.z), Quaternion.identity);
+        gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
     }
 
     private void OnDisable()
