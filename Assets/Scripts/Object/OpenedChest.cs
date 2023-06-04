451e437a68e6b797f078e95a84725d6e4eaec162
Buspe <93484404+AM-Dominik@users.noreply.github.com>
2023-06-04 21:05:09 +0200

    SimpleTale_#Chest_Drops_04.06.2023_21.05

--- Assets/Scripts/Object/OpenedChest.cs
+++ Assets/Scripts/Object/OpenedChest.cs
@@ -5,6 +5,8 @@ public class OpenedChest : MonoBehaviour
     [SerializeField] Sprite openedChest;
     private ITakeDamage health;
 
+    [SerializeField] private GameObject chestItem;
+
     void Start()
     {
         health = GetComponent<ITakeDamage>();
@@ -14,6 +16,7 @@ public class OpenedChest : MonoBehaviour
     private void OpenChest()
     {
         gameObject.GetComponent<SpriteRenderer>().sprite = openedChest;
+        Instantiate(chestItem, new Vector3(transform.position.x, (transform.position.y + 1), transform.position.z), Quaternion.identity);
     }
 
     private void OnDisable()
