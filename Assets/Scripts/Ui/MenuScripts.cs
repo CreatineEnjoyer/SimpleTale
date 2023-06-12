dedd159a91de4a58cd2767ecfc38f05870083d57
Buspe <93484404+AM-Dominik@users.noreply.github.com>
2023-06-12 15:43:38 +0200

    Updated Options Scripts

--- Assets/Scripts/Ui/MenuScripts.cs
+++ Assets/Scripts/Ui/MenuScripts.cs
@@ -53,26 +53,26 @@ public class MenuScripts : MonoBehaviour
         switch (windowSizeIndex)
         {
             case 0:
-                Screen.SetResolution(1920, 1080, true);
+                Screen.SetResolution(1920, 1080, Screen.fullScreen);
                 break;
 
             case 1:
-                Screen.SetResolution(640, 360, true);
+                Screen.SetResolution(640, 360, Screen.fullScreen);
 
                 break;
 
             case 2:
-                Screen.SetResolution(1280, 768, true);
+                Screen.SetResolution(1280, 768, Screen.fullScreen);
 
                 break;
 
             case 3:
-                Screen.SetResolution(1360, 768, true);
+                Screen.SetResolution(1360, 768, Screen.fullScreen);
 
                 break;
 
             case 4:
-                Screen.SetResolution(1600, 900, true);
+                Screen.SetResolution(1600, 900, Screen.fullScreen);
 
                 break;
 
